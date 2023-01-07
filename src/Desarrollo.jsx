591b1ef109a467b0b5ffff0eb21222a0230582e9
Sebastian Maciel <sebamaciel15@gmail.com>
2023-01-07 22:08:55 +0100

    adding folders and create component mercadolibreapp

--- src/Desarrollo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Desarrollo = () => {
-  return (
-    <div>Desarrollo Colaborativo</div>
-    )
-}
---
+++ src/MercadoLibreApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MercadoLibreApp = () => {
+    return (
+        <>
+            <h1>Aqui vamos a desarrollar la App Mercado Libre</h1>
+        </>
+      )
+  }
